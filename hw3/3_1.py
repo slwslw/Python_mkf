728603bbf069cb6c8971a4a47b29eda46422b2b4
slwslw <58246893+slwslw@users.noreply.github.com>
2024-02-25 01:18:14 +0300

    Delete hw3 directory

--- hw3/3_1.py
+++
@@ -1,7 +0,0 @@
-f = open('input.txt')
-lines = f.readline()
-for line in lines.split():
-    print(line[::-1], end=' ')
-f.close()
-
-
--- hw3/3_3.py
+++
@@ -1,8 +0,0 @@
-f = open('input.txt')
-lines = f.readlines()
-for line in lines:
-    a = line.split()
-    for str in a[::2]:
-        print(str, end=' ')
-    print()
-f.close()
